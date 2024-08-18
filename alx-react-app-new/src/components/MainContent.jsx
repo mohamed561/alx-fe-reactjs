a14eb6e28d90fc4b68da042e0ad2e5f18260b41b
mohamed561 <mrajawi561@gmail.com>
2024-08-18 19:09:19 +0100

    new

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,9 @@
 function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f0f0f0', margin: '20px 0' }}>
-      <p style={{ fontSize: '18px', lineHeight: '1.6' }}>I love to visit <span style={{ fontWeight: 'bold', color: 'green' }}>New York</span>, <span style={{ fontWeight: 'bold', color: 'blue' }}>Paris</span>, and <span style={{ fontWeight: 'bold', color: 'red' }}>Tokyo</span>.</p>
+      <p style={{ fontSize: '18px', lineHeight: '1.6' }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
     </main>
   );
 }
