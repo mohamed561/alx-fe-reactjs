26fc664b14a1d01f6c3de0a46094a56c8af53757
mohamed561 <mrajawi561@gmail.com>
2024-08-18 23:17:49 +0100

    m1

---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ background: '#333', color: 'white', textAlign: 'center', padding: '10px', position: 'fixed', bottom: 0, width: '100%' }}>
+      <p>&copy; 2023 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
