9358ab2c9b18c1cbd0efc9fd526e62d7406445ba
mohamed561 <mrajawi561@gmail.com>
2024-08-18 20:07:44 +0100

    error

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav style={{ padding: '10px', backgroundColor: '#333', color: 'white' }}>
-      <ul style={{ listStyleType: 'none', margin: 0, padding: 0, display: 'flex' }}>
-        <li style={{ margin: '0 10px' }}>
+      <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
+        <li style={{ display: 'inline', marginRight: '10px' }}>
           <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
         </li>
-        <li style={{ margin: '0 10px' }}>
+        <li style={{ display: 'inline', marginRight: '10px' }}>
           <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
         </li>
-        <li style={{ margin: '0 10px' }}>
+        <li style={{ display: 'inline', marginRight: '10px' }}>
           <Link to="/services" style={{ color: 'white', textDecoration: 'none' }}>Services</Link>
         </li>
-        <li style={{ margin: '0 10px' }}>
+        <li style={{ display: 'inline' }}>
           <Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link>
         </li>
       </ul>
