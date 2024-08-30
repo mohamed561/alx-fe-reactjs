793007cf0f85ac27d7bf395388673217ed2a0172
mohamed561 <mrajawi561@gmail.com>
2024-08-30 20:37:10 +0100

    just doing

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchPosts = async () => {
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return data;
+};
+
+const PostsComponent = () => {
+  const { data, isLoading, isError, error, refetch } = useQuery('posts', fetchPosts, {
+    staleTime: 60000, // 1 minute
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {error.message}</div>;
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <button onClick={() => refetch()}>Refetch Posts</button>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
