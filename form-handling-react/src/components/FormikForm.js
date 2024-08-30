88296031657341391d264bdc1a5a1f3dc877ea42
mohamed561 <mrajawi561@gmail.com>
2024-08-30 20:16:07 +0100

    m5

---
+++ form-handling-react/src/components/FormikForm.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string().required('Username is required'),
+  email: Yup.string().email('Invalid email').required('Email is required'),
+  password: Yup.string().required('Password is required')
+});
+
+const FormikForm = () => {
+  const initialValues = {
+    username: '',
+    email: '',
+    password: ''
+  };
+
+  const handleSubmit = (values, { setSubmitting }) => {
+    console.log('Form submitted:', values);
+    // Here you would typically send the data to your API
+    setSubmitting(false);
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <Field type="text" name="username" />
+            <ErrorMessage name="username" component="div" />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" />
+          </div>
+          <button type="submit" disabled={isSubmitting}>
+            Register
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default FormikForm;
