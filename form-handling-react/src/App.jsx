9907f7a8a8145cb882a0862bac363fb8ffae2be8
mohamed561 <mrajawi561@gmail.com>
2024-08-30 19:37:32 +0100

    m1

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/FormikForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Registration Form (Controlled Components)</h1>
+      <RegistrationForm />
+      <h1>Registration Form (Formik)</h1>
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
