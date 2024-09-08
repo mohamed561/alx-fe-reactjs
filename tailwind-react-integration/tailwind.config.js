decafd109784172c9dad3ad3f676cd21096384d0
mohamed561 <mrajawi561@gmail.com>
2024-09-08 18:12:24 +0100

    1

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"], // Explicitly adding purge
-  darkMode: 'media', // or 'class' if you want to toggle manually
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  darkMode: 'media',
   theme: {
     extend: {},
   },
