654d53f603060b11089051e9c632e591d2528a45
mohamed561 <mrajawi561@gmail.com>
2024-09-08 18:10:05 +0100

    1

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  darkMode: 'media',
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
