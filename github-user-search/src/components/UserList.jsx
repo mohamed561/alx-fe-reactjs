68813af48bd3e8850dd3d8015824de18742a4a69
mohamed561 <mrajawi561@gmail.com>
2024-09-20 17:40:52 +0100

    m1

---
+++ github-user-search/src/components/UserList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserCard from './UserCard';
+
+const UserList = ({ users }) => {
+  return (
+    <div className="user-list">
+      {users.map(user => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export default UserList;
