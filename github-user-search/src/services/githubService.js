809a83804ca0eb8c9acf34abc5bf7e295b3ca8e6
mohamed561 <mrajawi561@gmail.com>
2024-09-20 17:44:46 +0100

    m1

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.github.com';
+
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/users/${username}`, {
+      headers: {
+        Authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    throw error;
+  }
+};
